ed9a1087147b683cf5a7515e9442ca998837b100
sachin sharma <harshit0808sharma@gmail.com>
2023-06-03 11:38:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import React from "react"
+// import Card from "./Card";
+// import Contents from "./Contents";
+
+// const Movie = () =>{
+
+//     const movie = "Avengers";
+//     if(movie === "Avengers"){
+//         <Card
+//             imgsrc={Contents[0].imgsrc}
+//             title={Contents[0].title}
+//             sname={Contents[0].sname}
+//             link={Contents[0].link}
+//         />
+//     }else{
+//         <Card
+//             imgsrc={Contents[0].imgsrc}
+//             title={Contents[0].title}
+//             sname={Contents[0].sname}
+//             link={Contents[0].link}
+//         />
+//     }
+// }
+// const App = () =>{
+//  return(
+//         <Movie/>
+//     );
+// }
+// export default App;
